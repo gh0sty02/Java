4916bc6c1b6d0d09530897004db01a7c0d2bd0ac
gh0sty02 <pranayyadav-cmpn@atharvacoe.ac.in>
2021-10-10 20:55:25 +0530

    Collections framework

--- Course/Adventure/src/com/pranay/Basket.java
+++ Course/Adventure/src/com/pranay/Basket.java
@@ -1,6 +1,22 @@
 package com.pranay;
 
+import java.util.*;
+
 public class Basket {
     private final String name;
-    private Map
+    private final Map<StockItem, Integer> list;
+
+    public Basket(String name){
+        this.name = name;
+        this.list = new HashMap<>();
+    }
+
+    public int addToBasket(StockItem item, int quantity){
+        if((item != null) && quantity > 0){
+            int foundItem = list.getOrDefault(item, 0);
+            list.put(item, foundItem + quantity);
+            return foundItem;
+        }
+        return 0;
+    }
 }
